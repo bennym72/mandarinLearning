a4c154c78657df2ac0230961011c6ff3d4852fd3
Benny Mak <bennymak93@hotmail.com>
2025-01-25 13:50:54 -0800

    add ability to play text

--- main.js
+++ main.js
@@ -2219,6 +2219,109 @@ class TableBoard extends BaseBoard {
     }
 }
 
+// speech code
+
+
+class SpeechGeneration {
+
+    constructor() {
+        this._speechSynth = null;
+        this._voices = null;
+        this._cache = {};
+        this.retryCounter = 10;
+    }
+    
+    /**
+     * retries until there have been voices loaded. No stopper flag included in this example. 
+     * Note that this function assumes, that there are voices installed on the host system.
+     */
+
+    loadVoicesWhenAvailable (onComplete = () => {}) {
+        if (this.retryCounter <= 0) {
+            return;
+        }
+        this.retryCounter = this.retryCounter - 1;
+        this._speechSynth = window.speechSynthesis
+        const voices = this._speechSynth.getVoices()
+
+        if (voices.length !== 0) {
+            this._voices = voices
+            onComplete()
+        } else {
+            return setTimeout(function () { 
+                this.loadVoicesWhenAvailable(onComplete) 
+            }.bind(this), 100);
+        }
+    }
+
+    /**
+     * Returns the first found voice for a given language code.
+     */
+
+    getVoices (locale) {
+        if (!this._speechSynth) {
+            throw new Error('Browser does not support speech synthesis')
+        }
+        if (this._cache[locale]) {
+            return this._cache[locale]
+        }
+
+        this._cache[locale] = this._voices.filter(voice => voice.lang === locale)
+        return this._cache[locale]
+    }
+
+    /**
+     * Speak a certain text 
+     * @param locale the locale this voice requires
+     * @param text the text to speak
+     * @param onEnd callback if tts is finished
+     */
+
+    playByText (locale, text, onEnd) {
+        const voices = this.getVoices(locale)
+
+        // TODO load preference here, e.g. male / female etc.
+        // TODO but for now we just use the first occurrence
+        const utterance = new window.SpeechSynthesisUtterance()
+        utterance.voice = voices[0]
+        utterance.pitch = 1
+        utterance.rate = 1
+        utterance.voiceURI = 'native'
+        utterance.volume = 1
+        utterance.rate = 1
+        utterance.pitch = 0.8
+        utterance.text = text
+        utterance.lang = locale
+
+        if (onEnd) {
+            utterance.onend = onEnd
+        }
+
+        this._speechSynth.cancel() // cancel current speak, if any is running
+        this._speechSynth.speak(utterance)
+    }
+
+}
+
+let speechGenerator;
+function setupSpeechPlayer() {
+
+    speechGenerator = new SpeechGeneration();
+    // on document ready
+    speechGenerator.loadVoicesWhenAvailable(function () {
+        console.log("loaded") 
+    });
+}
+setupSpeechPlayer();
+
+function playCurrentSentence() {
+    
+    setTimeout(function playText() {
+        speechGenerator.playByText("en-US", "Hello, world")
+    }, 300)
+}
+
+
 // v2 code
 
 
